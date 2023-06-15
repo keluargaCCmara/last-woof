f61c33f4b5efb60cc1f212ae6bf2c7cab6c189ac
winxene <winxeneric@gmail.com>
2023-06-15 18:21:36 +0700

    feat: added Sprite Kit and storyboard

---
+++ LastWoof/GameViewController.swift
@@ -0,0 +1,42 @@
+//
+//  GameViewController.swift
+//  test-game
+//
+//  Created by Winxen Ryandiharvin on 15/06/23.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load 'GameScene.sks' as a GKScene. This provides gameplay related content
+        // including entities and graphs.
+        if let scene = GKScene(fileNamed: "GameScene") {
+            
+            // Get the SKScene from the loaded GKScene
+            if let sceneNode = scene.rootNode as! GameScene? {
+                // Set the scale mode to scale to fit the window
+                sceneNode.scaleMode = .aspectFill
+                
+                // Present the scene
+                if let view = self.view as! SKView? {
+                    view.presentScene(sceneNode)
+                    
+                    view.ignoresSiblingOrder = true
+                    
+                    view.showsFPS = true
+                    view.showsNodeCount = true
+                }
+            }
+        }
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
