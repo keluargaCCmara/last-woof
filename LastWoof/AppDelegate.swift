019d8c37e6dae685db9aaab475a5ef4df4244e48
Angelica Patricia Djaya Saputra <121279021+angelpatriciads@users.noreply.github.com>
2023-06-19 17:54:51 +0700

    Add Main Menu and Chapter 1

--- LastWoof/AppDelegate.swift
+++ LastWoof/AppDelegate.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-@main
+@UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
@@ -15,6 +15,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        window = UIWindow()
+        window?.makeKeyAndVisible()
+        
+        let viewController = GameViewController()
+        window?.rootViewController = viewController
+        
         return true
     }
 
--- LastWoof/GameViewController.swift
+++ LastWoof/GameViewController.swift
@@ -10,33 +10,25 @@ import SpriteKit
 import GameplayKit
 
 class GameViewController: UIViewController {
-
+    
+    lazy var skView: SKView = {
+      let view = SKView()
+      view.isMultipleTouchEnabled = true
+      return view
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Load 'GameScene.sks' as a GKScene. This provides gameplay related content
-        // including entities and graphs.
-        if let scene = GKScene(fileNamed: "GameScene") {
-            
-            // Get the SKScene from the loaded GKScene
-            if let sceneNode = scene.rootNode as! GameScene? {
-                // Set the scale mode to scale to fit the window
-                sceneNode.scaleMode = .aspectFill
-                
-                // Present the scene
-                if let view = self.view as! SKView? {
-                    view.presentScene(sceneNode)
-                    
-                    view.ignoresSiblingOrder = true
-                    
-                    view.showsFPS = true
-                    view.showsNodeCount = true
-                }
-            }
-        }
+        setupViews()
     }
 
-    override var prefersStatusBarHidden: Bool {
-        return true
+    fileprivate func setupViews() {
+      view.addSubview(skView)
+      
+      skView.frame = CGRect(x: 0.0, y: 0.0, width: ScreenSize.width, height: ScreenSize.height)
+      
+      let scene = MainMenuScene(size: CGSize(width: ScreenSize.width, height: ScreenSize.height))
+      scene.scaleMode = .aspectFill
+      skView.presentScene(scene)
     }
 }
