be937e4f5fbaace6be309413f569cf39da368d5c
Leonardo Wijaya <leonardo.wijaya003@binus.ac.id>
2023-06-21 00:45:13 +0700

    Object Collision Done
    It can kinda accurately handle the collision and the separation

--- LastWoof/Scenes/Scene1/GameScene.swift
+++ LastWoof/Scenes/Scene1/GameScene.swift
@@ -10,13 +10,14 @@ import GameplayKit
 
 struct PhysicsCategory {
     static let none: UInt32 = 0
-    static let character: UInt32 = 0b1
-    static let obstacle: UInt32 = 0b10
+    static let object: UInt32 = 0b1
+    static let character: UInt32 = 0b10
+    static let obstacle: UInt32 = 0b100
 }
 
 protocol PhysicsContactDelegate: AnyObject {
     func didBegin(_ contact: SKPhysicsContact)
-    func didEnd(_ contact: SKPhysicsContact)
+//    func didEnd(_ contact: SKPhysicsContact)
 }
 
 class GameScene: SKScene, SKPhysicsContactDelegate, PhysicsContactDelegate {
@@ -33,6 +34,9 @@ class GameScene: SKScene, SKPhysicsContactDelegate, PhysicsContactDelegate {
     
     private var lastTouchLocation: CGPoint?
     
+    private var isColliding: Bool = false
+    private var lastDidBeginTime: TimeInterval = 0
+    
     override func didMove(to view: SKView) {
         physicsWorld.contactDelegate = self
         guard let backgroundNode = childNode(withName: "background") as? SKSpriteNode else {
@@ -45,7 +49,6 @@ class GameScene: SKScene, SKPhysicsContactDelegate, PhysicsContactDelegate {
 //            VisualComponent(imageName: "DummyCharacter", size: CGSize(width: 200, height: 200), position: CGPoint(x: 140, y: -183), zPosition: 1, zRotation: 0, isDynamic: true, categoryBitMask: PhysicsCategory.character),
 //            MovementComponent(node: SKShapeNode(fileNamed: "DummyCharacter")!)
 //        ])
-        
         
         character = generateCharacter(imagedName: "DummyCharacter", width: 200, height: 200, xPosition: 140, yPosition: -183, zPosition: 1, zRotation: 0, isDynamic: true)
         addChild(character!)
@@ -65,7 +68,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate, PhysicsContactDelegate {
         ])
         
         let fence = generateEntity(components: [
-            VisualComponent(imageName: "Fence", size: CGSize(width: 1340, height: 2481), position: CGPoint(x: 2105, y: -519), zPosition: 1, zRotation: 0, isDynamic: false, categoryBitMask: PhysicsCategory.obstacle, collisionBitMask: PhysicsCategory.character, contactTestBitMask: PhysicsCategory.character)
+            VisualComponent(imageName: "Fence", size: CGSize(width: 1340, height: 2481), position: CGPoint(x: 2105, y: -519), zPosition: 1, zRotation: 0, isDynamic: false, categoryBitMask: PhysicsCategory.object, collisionBitMask: PhysicsCategory.character, contactTestBitMask: PhysicsCategory.character)
         ])
         
         entities = [pond, plant1, plant2, fence]
@@ -93,7 +96,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate, PhysicsContactDelegate {
         entity.physicsBody?.allowsRotation = allowsRotation
         entity.physicsBody?.affectedByGravity = false
         entity.physicsBody?.categoryBitMask = PhysicsCategory.character
-        entity.physicsBody?.collisionBitMask = PhysicsCategory.obstacle
+        entity.physicsBody?.collisionBitMask = PhysicsCategory.obstacle | PhysicsCategory.object
         entity.physicsBody?.contactTestBitMask = PhysicsCategory.obstacle
         entity.position = CGPoint(x: xPosition, y: yPosition)
         entity.zPosition = zPosition
@@ -202,20 +205,31 @@ class GameScene: SKScene, SKPhysicsContactDelegate, PhysicsContactDelegate {
         cameraNode.position = character!.position
         scene?.camera = cameraNode
         boundsCheckCamera()
+        let elapsedDidBeginTime = currentTime - lastDidBeginTime
+        if elapsedDidBeginTime > 0.5 {
+            isColliding = false
+            print("Character separated with obstacle")
+        }
     }
         
     // MARK: PhysicsContactDelegate Protocol
     func didBegin(_ contact: SKPhysicsContact) {
         let collision = contact.bodyA.categoryBitMask | contact.bodyB.categoryBitMask
+        self.lastDidBeginTime = CACurrentMediaTime()
 
         if collision == PhysicsCategory.character | PhysicsCategory.obstacle {
+            isColliding = true
             handleCharacterObstacleCollision(contact: contact)
         }
     }
     
-    func didEnd(_ contact: SKPhysicsContact) {
-        print("collision ended")
-    }
+//    func didEnd(_ contact: SKPhysicsContact) {
+//        let collision = contact.bodyA.categoryBitMask | contact.bodyB.categoryBitMask
+//
+//        if collision == PhysicsCategory.character | PhysicsCategory.obstacle {
+////            handleCharacterObstacleSeparation(contact: contact)
+//        }
+//    }
     
     private func handleCharacterObstacleCollision(contact: SKPhysicsContact) {
         let characterNode = contact.bodyA.categoryBitMask == PhysicsCategory.character ? contact.bodyA.node : contact.bodyB.node
@@ -223,5 +237,18 @@ class GameScene: SKScene, SKPhysicsContactDelegate, PhysicsContactDelegate {
         
         // Perform actions or logic when character collides with an obstacle
         print("Character collided with obstacle")
+        
+    }
+    
+    private func handleCharacterObstacleSeparation(contact: SKPhysicsContact) {
+        print("Character separated with obstacle")
+    }
+}
+
+extension CGPoint {
+    func distance(to point: CGPoint) -> CGFloat {
+        let dx = point.x - self.x
+        let dy = point.y - self.y
+        return sqrt(dx*dx + dy*dy)
     }
 }
