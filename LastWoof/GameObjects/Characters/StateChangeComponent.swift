b8c70539b76867d4f2afc37f5c13b29b43c7e984
Angela <angela.christabel@gmail.com>
2023-06-21 14:23:31 +0700

    add scenes for stories

---
+++ LastWoof/GameObjects/Characters/StateChangeComponent.swift
@@ -0,0 +1,51 @@
+//
+//  ChangeStateComponent.swift
+//  Last Woof
+//
+//  Created by Angela Christabel on 18/06/23.
+//
+
+import Foundation
+import SpriteKit
+import GameplayKit
+
+enum State {
+    case fade, texture
+}
+
+class StateChangeComponent: GKComponent {
+    // MARK: Properties
+
+    /// A convenience property for the entity's visual component
+    var visualComponent: VisualComponent? {
+        return entity?.component(ofType: VisualComponent.self)
+    }
+
+    // MARK: Methods
+    func changeState(mode: State, _ texture: SKTexture?) {
+        if mode == .fade {
+            fade()
+        } else {
+            changeTexture(texture: texture)
+        }
+    }
+    
+    /// Tells this entity's visual  component to fade.
+    func fade() {
+        let fadeAction = SKAction.fadeOut(withDuration: 1.5)
+        visualComponent?.node.run(fadeAction)
+    }
+    
+    /// Tells this entity's visual component to change texture/state
+    func changeTexture(texture: SKTexture?) {
+        guard let node = visualComponent?.node else {return}
+        
+//        if node.state == 1 {
+//            if let texture = texture {
+//                node.state = 2
+//                node.texture = texture
+//            }
+//        }
+    }
+}
+
